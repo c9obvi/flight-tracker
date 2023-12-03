8af9e5bdb5b37ebbb7d2c3bf16085926448ca8d6
alberto septien <c9obvi@gmail.com>
2023-12-02 21:38:55 -0800

    initial code commit

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# flight-tracker
+# Plane Information Scraper
+
+## Overview
+This tool is designed to scrape plane information from the website "ADS-B Exchange". It captures various details such as Groundspeed, Barometric Altitude, WGS84 Altitude, Vertical Rate, and more. The script is implemented in Python and uses Selenium for web scraping and BeautifulSoup for HTML parsing.
+
+## Features
+- Scrape plane information from ADS-B Exchange.
+- Extract specific data like Groundspeed, Altitude, Track, Position, and more.
+- Display elapsed time during scraping with a threading-based timer.
+- Save scraped data in JSON format.
+
+## Prerequisites
+- Python 3.x
+- Selenium
+- BeautifulSoup4
+- ChromeDriver (compatible with the installed Chrome version)
+
+## Installation
+1. Clone this repository or download the script.
+2. Install the required Python packages:
+   ```
+   pip install selenium beautifulsoup4
+   ```
+3. Download ChromeDriver from [here](https://sites.google.com/a/chromium.org/chromedriver/downloads) and place it in a known directory.
+
+## Usage
+1. Open the script in a text editor and set the target URL in the `url` variable.
+2. Run the script using Python:
+   ```
+   python plane_scraper.py
+   ```
+3. Check the terminal for the elapsed time and the output directory for the generated JSON file named `plane_data-main.json`.
+
+## Output
+The script saves the scraped data in a JSON file named `plane_data-main.json`. This file includes details like Groundspeed, Barometric Altitude, Track, etc., in a structured format.
+
+## Contributing
+Contributions, issues, and feature requests are welcome! Feel free to fork the repository and submit pull requests.
+
+## License
+Distributed under the MIT License. See `LICENSE` for more information.
+
+## Contact
+Your Name - [Your Email](mailto:youremail@example.com)
+
+Project Link: [https://github.com/yourusername/your-repo-name](https://github.com/yourusername/your-repo-name)
---
+++ main-timed.py
@@ -0,0 +1,75 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+import time
+import json
+import threading
+
+# Timer function to display elapsed time
+def display_timer():
+    start_time = time.time()
+    while not stop_timer_event.is_set():
+        elapsed_time = time.time() - start_time
+        print(f"Elapsed time: {elapsed_time:.2f} seconds", end='\r')
+        time.sleep(1)
+
+# URL of the website
+url = "https://globe.adsbexchange.com/?icao=86d5be"
+
+# Setup Selenium WebDriver
+options = Options()
+options.headless = True  # Run in headless mode
+driver = webdriver.Chrome(options=options)
+
+# Function to scrape data
+def scrape_plane_info(url):
+    driver.get(url)
+    time.sleep(7)  # Wait for the page to load
+
+    # Using BeautifulSoup to parse the page source
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+    # Dictionary to hold the scraped data
+    plane_info = {}
+
+    # Extracting altitude information
+    plane_info['Groundspeed'] = soup.find(id='selected_speed1').get_text(strip=True) if soup.find(id='selected_speed1') else 'n/a'
+    plane_info['Baro. Altitude'] = soup.find(id='selected_altitude1').get_text(strip=True) if soup.find(id='selected_altitude1') else 'n/a'
+    plane_info['WGS84 Altitude'] = soup.find(id='selected_altitude_geom1').get_text(strip=True) if soup.find(id='selected_altitude_geom1') else 'n/a'
+    plane_info['Vert. Rate'] = soup.find(id='selected_vert_rate').get_text(strip=True) if soup.find(id='selected_vert_rate') else 'n/a'
+    plane_info['Track'] = soup.find(id='selected_track1').get_text(strip=True) if soup.find(id='selected_track1') else 'n/a'
+    plane_info['Position'] = soup.find(id='selected_position').get_text(strip=True) if soup.find(id='selected_position') else 'n/a'
+    plane_info['Distance'] = soup.find(id='selected_sitedist2').get_text(strip=True) if soup.find(id='selected_sitedist2') else 'n/a'
+    plane_info['Source'] = soup.find(id='selected_source').get_text(strip=True) if soup.find(id='selected_source') else 'n/a'
+    plane_info['RSSI'] = soup.find(id='selected_rssi1').get_text(strip=True) if soup.find(id='selected_rssi1') else 'n/a'
+    plane_info['Message Rate'] = soup.find(id='selected_message_rate').get_text(strip=True) if soup.find(id='selected_message_rate') else 'n/a'
+    plane_info['Last Position'] = soup.find(id='selected_seen_pos').get_text(strip=True) if soup.find(id='selected_seen_pos') else 'n/a'
+    plane_info['Last Seen'] = soup.find(id='selected_seen').get_text(strip=True) if soup.find(id='selected_seen') else 'n/a'
+    # ... Continue for other fields as needed
+
+    return plane_info
+
+# Start the timer
+stop_timer_event = threading.Event()
+timer_thread = threading.Thread(target=display_timer)
+timer_thread.start()
+
+# Scrape data from the URL
+plane_info = scrape_plane_info(url)
+
+# Convert to JSON
+json_data = json.dumps(plane_info, indent=4)
+
+# Close the driver
+driver.quit()
+
+# Stop the timer
+stop_timer_event.set()
+timer_thread.join()
+
+# Print the JSON data
+print('\n' + json_data)
+print('Flight information has been saved to plane_data-main.json')
+
+with open('plane_data-timed.json', 'w') as file:
+    json.dump(plane_info, file, indent=4)
